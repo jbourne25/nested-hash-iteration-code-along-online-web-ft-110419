bddd3d8bf8afdb7163583c7e1eaa7574ecd1616d
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-11 17:00:01 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -24,7 +24,7 @@ require 'pry'
 #     end 
 #   end 
 def remove_strawberry(contacts)  
-  contacts.each do |person, contact_details_hash|
+contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       binding.pry
